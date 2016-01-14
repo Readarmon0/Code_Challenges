1abaa1a50034e09f1389508f7172afcadf399974
Readarmon0 <Readarmon0@hotmail.com>
2016-01-14 07:33:14 -0600

    Signed-off-by: Readarmon0 <Readarmon0@hotmail.com>

---
+++ rust/embed/target/debug/embed.d
@@ -0,0 +1,5 @@
+C:\Users\John\Documents\GitHub\Code_Challenges\rust\embed\target\debug\embed.d: src\main.rs
+
+C:\Users\John\Documents\GitHub\Code_Challenges\rust\embed\target\debug\embed.exe: src\main.rs
+
+src\main.rs:
