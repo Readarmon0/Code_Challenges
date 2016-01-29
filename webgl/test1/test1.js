7449f5e11eac9b99ffe692068e56c7346e2074d7
Readarmon0 <Readarmon0@hotmail.com>
2016-01-29 12:41:19 -0600

    Signed-off-by: Readarmon0 <Readarmon0@hotmail.com>

--- webgl/test1/test1.js
+++ webgl/test1/test1.js
@@ -1,10 +1,9 @@
-var VSHADER_SOURCE =
+var VSHADER_SOURCE = 
 	'attribute vec4 a_Position;\n' +
-	'attribute float a_PointSize;\n' +
-	'void main() {\n' + 
-	'	gl_Position = a_Position;\n' + // Coordinates
-	'	gl_PointSize = a_PointSize;\n' + // Set the point size
-    '}\n';
+	'void main() {\n' +
+	'	gl_Position = a_Position;\n' +
+	'	gl_PointSize = 10.0;\n' +
+	'}\n';
 // Fragment shader program
 var FSHADER_SOURCE =
 	'void main() {\n' +
@@ -24,12 +23,15 @@ function main() {
 		console.log('Failed to initialize shaders.');
 		return;
 	}
-	// Get the storage location of attribute variable
+	// Get the storage location of a a_Position variable
 	var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
 	if (a_Position < 0) {
 		console.log('Failed to get the storage location of a_Position');
 		return;
 	}
+	// ...
+	// Register function (event handler) to be called to a mouse press
+	canvas.onmousedown = function(ev) { click(ev, gl, canvas, a_Position); };
 	var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
 	// Set vertex position to attribute variable
 	gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
@@ -40,6 +42,25 @@ function main() {
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	// Draw a point
 	gl.drawArrays(gl.POINTS, 0, 1);
+}
+var g_points = []; // The array for a mouse press
+function click(ev, gl, canvas, a_Position) {
+	var x = ev.clientX; // x coordinate of a mouse pointer
+	var y = ev.clientY; // y coordinate of a mouse pointer
+	var rect = ev.target.getBoundingClientRect();
+	x = ((x - rect.left) - canvas.height / 2) / (canvas.height / 2);
+	y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
+	// Store the coordinates to g_points array
+	g_points.push(x); g_points.push(y);
+	// Clear <canvas>
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	var len = g_points.length;
+	for (var i = 0; i < len; i += 2) {
+		// Pass the position of a point to a_Position variable
+		gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+		// Draw a point
+		gl.drawArrays(gl.POINTS, 0, 1);
+	}
 }
 
 /*
